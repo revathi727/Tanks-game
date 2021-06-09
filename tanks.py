e125fbfbec4768273503cdfae4ef756c79cf0862
revathi727 <revathipolisetty7@gmail.com>
2021-06-09 14:31:14 +0530

    73 Adding Ground

--- tanks.py
+++ tanks.py
@@ -35,6 +35,8 @@ tankHeight = 20
 turretWidth = 5
 wheelWidth = 5
 
+ground_height = 35
+
 smallfont = pygame.font.SysFont("comicsansms", 25)
 medfont = pygame.font.SysFont("comicsansms", 50)
 largefont = pygame.font.SysFont("comicsansms", 80)
@@ -205,10 +207,10 @@ def fireShell(xy, tankx, tanky, turPos, gun_power, xlocation, barrier_width, ran
         # y = x**2
         startingShell[1] += int(((startingShell[0]-xy[0])*0.015/(gun_power/50))**2 - (turPos+turPos/(12-turPos)))
 
-        if startingShell[1] > display_height:
+        if startingShell[1] > display_height-ground_height:
             print("Last Shell:", startingShell[0], startingShell[1])
-            hit_x = int((startingShell[0]* display_height)/startingShell[1])
-            hit_y = int(display_height)
+            hit_x = int((startingShell[0]* display_height-ground_height)/startingShell[1])
+            hit_y = int(display_height-ground_height)
             print("Impact:", hit_x,hit_y)
             explosion(hit_x, hit_y)
             fire = False
@@ -358,6 +360,7 @@ def gameLoop():
         power(fire_power)
 
         barrier(xlocation, randomHeight, barrier_width)
+        gameDisplay.fill(green, rect=[0, display_height-ground_height, display_width, ground_height])
         pygame.display.update()
 
         clock.tick(FPS)
